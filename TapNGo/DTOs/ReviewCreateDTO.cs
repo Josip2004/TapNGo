38f3d0455c9fdec5e882cc08c96d1d430e18244e
IvanPetar <ivanpetarsolic1@gmail.com>
2025-04-22 23:01:12 +0200

    Add try/catch in review, and change dtos

--- TapNGo/DTOs/ReviewCreateDTO.cs
+++ TapNGo/DTOs/ReviewCreateDTO.cs
@@ -8,8 +8,9 @@ namespace TapNGo.DTOs
         public int UserId { get; set; }
 
         [Range(1,5,ErrorMessage = "Review must be between 1 - 5.")]
+        [Required(ErrorMessage = "You must leave the rating")]
         public int? Rating { get; set; }
-
+        [Required(ErrorMessage = "You must leave comment")]
         public string? Comment { get; set; }
     }
 }
